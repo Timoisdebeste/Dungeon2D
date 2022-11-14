087492a69c418e70537f73698751713cae2c9426
patrick <260761@cursist.rockopnh.nl>
2022-11-14 15:57:34 +0100

    enemy

--- rpg/Assets/scripts/AIchase.cs
+++ rpg/Assets/scripts/AIchase.cs
@@ -21,7 +21,11 @@ public class AIchase : MonoBehaviour
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if(distance < 4)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        } 
+        
 
     }
 }
