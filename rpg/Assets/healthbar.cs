241bd4d351899c9bd5665577602593dcc8958492
Jemm <jemm716@gmail.com>
2022-11-16 13:48:07 +0100

    a

---
+++ rpg/Assets/healthbar.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+
+public Slider Slider;
+
+public void Sethealth(int health)
+{
+    Slider.value = health
+}
+}
